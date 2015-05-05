678b4b04f24a765ae809f2bd60bf42e7328800b4
Ido <i.sternberg@posteo.de>
2015-05-05 15:04:25 +0200

    added part 2. not sure if implementation correct

--- multiVariateLineareRegession.py
+++ multiVariateLineareRegession.py
@@ -3,17 +3,15 @@ from matplotlib import pylab as plt
 import numpy as np
 x_min = -10.
 x_max = 10.
-m = 11
-x = np.array([np.random.uniform(x_min, x_max, m),np.random.uniform(x_min, x_max, m)])
-print(x)
+m = 2
+xList = np.random.uniform(x_min, x_max, m)
+xList[0] = 1 
+print(xList)
 
 # 2
-
-#def linear_hypothesis(theta_0, theta_1):
-#    return lambda x:theta_0 + theta_1 * x #Ebenengleichung
+thetas = [2,3]
 def linear_hypothesis(thetas):
-	return lambda xList: np.sum([ theta * x for theta, x in zip(thetas, xList) ])
+	return lambda xList: np.sum([ theta * x for theta, x in zip(thetas, xList)])
 
-h = linear_hypothesis(theta_0=1, theta_1=2)
-print(h(1)) # => 3 bester Y-Wert fuer einen x-Wert
-print(h(np.array([1., 2.])))
+h = linear_hypothesis(thetas)
+print(h(x))
