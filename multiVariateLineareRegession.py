9e672d145e6cb4d0fe7f810c67493d46d5bc8060
Ido <i.sternberg@posteo.de>
2015-05-12 15:37:49 +0200

    added part 2 and 3

--- multiVariateLineareRegession.py
+++ multiVariateLineareRegession.py
@@ -1,17 +1,42 @@
 # 1 
 from matplotlib import pylab as plt
 import numpy as np
+from mpl_toolkits.mplot3d import Axes3D
 x_min = -10.
 x_max = 10.
-m = 2
-xList = np.random.uniform(x_min, x_max, m)
-xList[0] = 1 
-print(xList)
+m = 100
+features_count = 2
+features_with_X0 = features_count + 1 # add col x0 zu every row
+xLists = np.array([np.random.uniform(x_min, x_max, features_with_X0) for x in range(m)])
+
+xLists[:,0] =1.0 # set the 1. col to 1.0
+
+print(xLists)
 
 # 2
-thetas = [2,3]
-def linear_hypothesis(thetas):
-	return lambda xList: np.sum([ theta * x for theta, x in zip(thetas, xList)])
 
+thetas = np.array([1.1, 2.0, -.9])
+def linear_hypothesis(thetas):
+	return lambda xLists: xLists.dot(thetas)
+#3 a
 h = linear_hypothesis(thetas)
-print(h(x))
+y = h(xLists)
+print("Y-values: ")
+print(y)
+print("Y-values with gauss rauschen:")
+y_noise_simga = y * 0.1
+y_new = y + np.random.randn(m) * y_noise_simga
+print(y_new)
+
+#3 b
+print("X1: ")
+x1 = xLists[:,1]
+
+print(x1)
+print("X2: ")
+x2 = xLists[:,2]
+print(x2)
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.scatter(x1, x2, y_new)
+
