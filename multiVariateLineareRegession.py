75d86418d48025785ee7f7624c931cd2e81a1c03
Ido <i.sternberg@posteo.de>
2015-05-12 22:14:02 +0200

    added part 4. need to see if result of cost function makes sense.

--- multiVariateLineareRegession.py
+++ multiVariateLineareRegession.py
@@ -4,14 +4,14 @@ import numpy as np
 from mpl_toolkits.mplot3d import Axes3D
 x_min = -10.
 x_max = 10.
-m = 100
+m = 4
 features_count = 2
 features_with_X0 = features_count + 1 # add col x0 zu every row
 xLists = np.array([np.random.uniform(x_min, x_max, features_with_X0) for x in range(m)])
 
 xLists[:,0] =1.0 # set the 1. col to 1.0
 
-print(xLists)
+#print(xLists)
 
 # 2
 
@@ -21,22 +21,39 @@ def linear_hypothesis(thetas):
 #3 a
 h = linear_hypothesis(thetas)
 y = h(xLists)
-print("Y-values: ")
-print(y)
-print("Y-values with gauss rauschen:")
+#print("Y-values: ")
+#print(y)
+#print("Y-values with gauss rauschen:")
 y_noise_simga = y * 0.1
 y_new = y + np.random.randn(m) * y_noise_simga
-print(y_new)
+#print(y_new)
 
 #3 b
-print("X1: ")
+#print("X1: ")
 x1 = xLists[:,1]
 
-print(x1)
-print("X2: ")
+#print(x1)
+#print("X2: ")
 x2 = xLists[:,2]
-print(x2)
+#print(x2)
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 ax.scatter(x1, x2, y_new)
+
+#4 cost function
+def cost_function(x, y):
+    assert(len(x) == len(y))
+    m = len(x)
+    return lambda thetas: 1. / ((2. * float(m)) * (linear_hypothesis(thetas)(x) - y) ** 2).sum()
+
+j = cost_function(xLists,y_new)
+print(j(thetas))
+
+
+
+
+
+
+
+
 
