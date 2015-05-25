d8b91ee970e9f6336fec30ea74b22c2c663812d1
Anja Kammer <anja@debian.de>
2015-05-26 01:45:11 +0200

    finished 1-6

--- multiVariateLineareRegession.py
+++ multiVariateLineareRegession.py
@@ -1,55 +1,52 @@
-# 1 
+# 1
 from matplotlib import pylab as plt
 import numpy as np
 from mpl_toolkits.mplot3d import Axes3D
 
 x_min = -10.
 x_max = 10.
-m = 4
+m = 100
 features_count = 2
-features_with_X0 = features_count + 1  # add col x0 zu every row
+features_with_X0 = features_count + 1
+# np.concatenate
 xLists = np.array([np.random.uniform(x_min, x_max, features_with_X0) for x in range(m)])
+xLists[:, 0] = 1.0  # first column to 1.0
 
-xLists[:, 0] = 1.0  # set the 1. col to 1.0
 
-# print(xLists)
-
-#xList.reshape([len(xList), 1]).shape
-# im Scatter plot darstellen - z-label ist das y, X und Y Label sind die x-werte
 
 # 2
-
 thetas = np.array([1.1, 2.0, -.9])
 
 
 def linear_hypothesis(thetas):
-    return lambda xLists: xLists.dot(thetas)
+    return lambda xLists: xLists.dot(np.transpose(thetas))
 
-#3 a
+
 h = linear_hypothesis(thetas)
+print(h(xLists))
 
-x = [1, 2]
-print(h(x))
 
+# 3 a
+#y = h(xLists) + 2. * np.random.randn(m)
 y = h(xLists)
-#print("Y-values: ")
-#print(y)
-#print("Y-values with gauss rauschen:")
+print("Y-values: \n" + str(y))
+
 y_noise_simga = y * 0.1
 y_new = y + np.random.randn(m) * y_noise_simga
-#print(y_new)
+print(y_new)
+print("Y-values with gauss rauschen: \n" + str(y_new))
 
 #3 b
-#print("X1: ")
-x1 = xLists[:, 1]
+x1 = xLists[:, 1]  # 1 vector
+print("x1-values: \n" + str(x1))
 
-#print(x1)
-#print("X2: ")
-x2 = xLists[:, 2]
-#print(x2)
+x2 = xLists[:, 2]  # 2 vector
+print("x2-values: \n" + str(x2))
+
 fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
+ax = fig.add_subplot(111, projection='3d')  # 1x1 grid, 1 subplot
 ax.scatter(x1, x2, y_new)
+plt.show()
 
 #4 cost function
 def cost_function(x, y):
@@ -59,11 +56,41 @@ def cost_function(x, y):
 
 
 j = cost_function(xLists, y_new)
-print(j(thetas))
+print("cost-function: \n" + str(j(thetas)))
+
+# 5a
+def compute_new_theta(x, y, thetas, alpha):
+    m = y.size
+    return thetas - alpha * (1. / m) * \
+                    x.transpose().dot(linear_hypothesis(thetas)(x) - y)
 
 
+# 5b
+def gradient_decent(alpha, theta, nb_iterations, x, y):
+    cost_f = cost_function(x, y)
+    costs = np.zeros(nb_iterations)
+    for i in range(nb_iterations):
+        new_thetas = compute_new_theta(x, y, theta, alpha)
+        costs[i] = cost_f(new_thetas)
+    return new_thetas, costs
 
+alpha = 0.01
+iter = 1000     # iterationen
+new_thetas, costs = gradient_decent(alpha, thetas, iter, xLists, y_new)
+print new_thetas # neue Thetas
 
+# 5c
+fig = plt.figure()
+ax = fig.add_subplot(111)
+plt.plot(np.array(range(iter)), costs)
+plt.show()
+
+# 6
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.plot(xLists[:, 1], xLists[:, 2], linear_hypothesis(new_thetas)(xLists))
+ax.scatter(xLists[:, 1], xLists[:, 2], y_new)
+plt.show()
 
 
 
