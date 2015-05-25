ac1f14dcf5284f5447e61b3b99b54a3da9ef577a
Anja Kammer <anja@debian.de>
2015-05-26 00:02:41 +0200

    multiVariateLinRegress

--- multiVariateLineareRegession.py
+++ multiVariateLineareRegession.py
@@ -8,10 +8,16 @@ xList = np.random.uniform(x_min, x_max, m)
 xList[0] = 1 
 print(xList)
 
+xList.reshape([len(xList), 1]).shape
+# im Scatter plot darstellen - z-label ist das y, X und Y Label sind die x-werte
+
 # 2
 thetas = [2,3]
 def linear_hypothesis(thetas):
 	return lambda xList: np.sum([ theta * x for theta, x in zip(thetas, xList)])
 
 h = linear_hypothesis(thetas)
+x = [1,2]
 print(h(x))
+
+
