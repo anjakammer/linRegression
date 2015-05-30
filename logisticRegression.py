fcf835b9cc144f0711c50d1c47b345235c5dcecf
Anja Kammer <anja@debian.de>
2015-05-30 17:30:58 +0200

    task 1to3

--- logisticRegression.py
+++ logisticRegression.py
@@ -3,12 +3,32 @@ from matplotlib import pylab as plt
 import numpy as np
 from mpl_toolkits.mplot3d import Axes3D
 
+#1
 def logistic_function(z):
-    return 1 / (1 + np.exp(-z ))
+    return float(1 / (1 + np.exp(-z)))
 
+iterations = 1000
+plt.axis([-5, 5, 0, 1])
+output = np.empty(iterations)
+input = np.empty(iterations)
 
-range = np.array(range(10))
-n = 42
-print(logistic_function(n))
-plt.plot(1., 10.)
+for i in range(iterations):
+    input[i] = float((i/100) - 5.0)
+    output[i] = (logistic_function(input[i]))
+
+plt.plot(input, output)
 plt.show()
+
+#2
+thetas = np.array([1.1, 2.0, -.9])
+
+def logistic_hypothesis(thetas):
+    return lambda x: 1 / (1 + np.exp(-thetas.transpose()*x))
+
+h = logistic_hypothesis(thetas)
+x = np.array([-5, 0, 5])
+print(h(x))
+
+#3
+def cross_entropy_loss(h, x, y):
+    return lambda theta: 1/2 * (h(theta)(x)-y)**2
